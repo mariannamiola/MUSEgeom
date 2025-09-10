da7fdcab36e838e3aabbeaebe506babe4f10f57f
MariannaMiola <miola.marianna@gmail.com>
2025-09-10 17:41:33 +0200

    updated

---
+++ examples/script/01_Portofino.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e	#exit if an error occours
+
+export EXE=MUSEgeom
+export PROJECT_NAME=01_Portofino
+
+# 0. Export paths
+#######################################################################
+
+#### Directiory (full path) where this scripts lies
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BIN_DIR=${SCRIPT_DIR}/../../bin
+export PATH=${BIN_DIR}:$PATH
+
+export TOOL=${SCRIPT_DIR}/../..
+export DATA_SOURCE=${TOOL}/examples/data ##data directory: where input data lies
+
+export WORK_DIR=${TOOL}/examples/run ##project directory: where results lies
+mkdir -p ${WORK_DIR}
+
+export WP=${WORK_DIR}/${PROJECT_NAME}
+
+#######################################################################
+
+
+# 1. Set input data
+export BOUNDARY=AOI
+export FORMAT0=gpkg
+
+export BATIM=portofino_batimetria_LR_clean
+export FORMAT1=xyz
+
+export INWP=${WP}/in
+mkdir -p ${INWP}
+
+cp ${DATA_SOURCE}/${BOUNDARY}.* ${INWP}
+cp ${DATA_SOURCE}/${BATIM}.* ${INWP}
+
+#######################################################################
+
+
+
+# 2. Set flags
+#######################################################################
+export OPT=a25
+
+
+# 3. Starting script ...
+########## REPORT  ###########
+
+echo "
+
+    	Running test: $PROJECT_NAME 
+    			[ datasource 	    ] ${DATA_SOURCE}
+    			[ project directory ] ${WP}
+    			
+               		"
+export OUTSURF=${WP}/out/surf
+
+##Reading vector file and extracting boundary points
+${EXE} -V -p ${WP} --save --xyz
+mv ${OUTSURF}/${BOUNDARY}_0@${FORMAT0}.xyz ${OUTSURF}/${BOUNDARY}.xyz
+
+##Reading batimetry as point cloud
+${EXE} -P -p ${WP} --points ${INWP}/${BATIM}.${FORMAT1} --boundary ${OUTSURF}/${BOUNDARY}.xyz --tri --obj
+
+##Surface offset
+#${EXE} -O -p ${WP} -m ${OUTSURF}/${INPUT}.obj --delta -z ${HALF_RESZ} --obj
+#mv ${OUTSURF}/${INPUT}_dz.obj ${OUTSURF}/${INPUT}_dzsup.obj
+
+
+
+##Lateral closure of the two surfaces
+#${EXE} -T -p ${WP} -m ${OUTSURF}/${INPUT}_dzsup.obj -m ${OUTSURF}/${INPUT}_dzinf.obj --obj
+
+##Hexahedral meshing
+#${EXE} -M -p ${WP} -m ${OUTSURF}/${INPUT}_closed.obj --hex --resx ${RESX} --resy ${RESY} --resz ${RESZ} --vtk
+
