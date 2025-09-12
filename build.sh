787e1881c1378af6003c3dd81c65e08a857e52b2
Marianna Miola <marianna.miola@cnr.it>
2025-09-12 22:33:51 +0200

    updated

--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ set -e	#exit if an error occours
 #### Directiory (full path) where this script lies
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+#####
+# Detect OS
+OS=$(uname)
+LIB_EXT="so"  # Linux by default
+[[ "$OS" == "Darwin" ]] && LIB_EXT="dylib"
+echo "=== Detected OS: $OS"
+
+
 # Parse command line argument
 BUILD_ALL=true
 if [[ "$1" == "--only-tool" ]]; then
@@ -31,9 +39,24 @@ if $BUILD_ALL; then
              -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
              -DOGR_ENABLE_DRIVER_GPKG=ON \
              -DPROJ_INCLUDE_DIR=${PROJ}/installed/include \
-             -DPROJ_LIBRARY_RELEASE=${PROJ}/installed/lib/libproj.so \
+             -DPROJ_LIBRARY_RELEASE=${PROJ}/installed/lib/libproj.${LIB_EXT} \
              -DBUILD_PYTHON_BINDINGS=OFF \
              -DCMAKE_BUILD_TYPE=Release
+    cmake --build . --parallel 16
+    cmake --install .
+    
+    ###VTK
+    export VTK=${SCRIPT_DIR}/external/VTK
+    cd ${VTK}
+    mkdir -p build
+    cd build
+
+    cmake .. -DCMAKE_BUILD_TYPE=Release \
+             -DCMAKE_INSTALL_PREFIX=${VTK}/installed \
+             -DVTK_BUILD_TESTING=OFF \
+             -DVTK_GROUP_ENABLE_Qt=NO \
+             -DBUILD_SHARED_LIBS=ON
+
     cmake --build . --parallel 16
     cmake --install .
 fi
